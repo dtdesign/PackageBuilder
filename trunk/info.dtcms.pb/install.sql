38876dd7a36292847e6cd821367f4b8f5c1817aa
Alexander Ebert <alexander.ebert@deltateam-cms.info>
2010-02-22 23:33:55 +0100

    Removed subversion related columns and tables, replace with scm

--- trunk/info.dtcms.pb/install.sql
+++ trunk/info.dtcms.pb/install.sql
@@ -3,16 +3,17 @@ CREATE TABLE pb1_1_sources (
 	name VARCHAR( 80 ) NOT NULL ,
 	sourceDirectory TEXT NOT NULL ,
 	buildDirectory TEXT NOT NULL,
+	scm ENUM('git','subversion') NOT NULL,
 	useSubversion TINYINT( 1 ) NOT NULL ,
 	url TEXT NULL ,
 	username VARCHAR( 80 ) NULL ,
-	revision SMALLINT UNSIGNED NOT NULL ,
+	revision VARCHAR(40) NOT NULL ,
 	trustServerCert TINYINT( 1 ) unsigned NOT NULL ,
 	sortOrder VARCHAR(4) NOT NULL ,
 	password VARCHAR( 80 ) NULL ,
 	PRIMARY KEY ( sourceID ),
 	INDEX sortOrder (sortOrder)
-) ENGINE = MYISAM CHARACTER SET utf8;
+) ENGINE=MyISAM CHARACTER SET=utf8;
 
 CREATE TABLE pb1_1_sources_packages (
 	sourceID INT( 10 ) NOT NULL ,
@@ -22,7 +23,7 @@ CREATE TABLE pb1_1_sources_packages (
 	directory MEDIUMTEXT NOT NULL ,
 	INDEX ( sourceID ),
 	UNIQUE ( hash )
-) ENGINE = MYISAM CHARACTER SET utf8;
+) ENGINE=MyISAM CHARACTER SET=utf8;
 
 CREATE TABLE pb1_1_referenced_packages (
 	sourceID INT( 10 ) NOT NULL ,
@@ -42,9 +43,3 @@ CREATE TABLE pb1_1_selected_packages (
 	resourceDirectory MEDIUMTEXT NOT NULL,
 	UNIQUE KEY packageKey (sourceID, directory(250), hash)
 ) ENGINE=MyISAM CHARACTER SET=utf8;
-
-CREATE TABLE pb1_1_subversion (
-	sourceID INT( 10 ) NOT NULL ,
-	message MEDIUMTEXT NOT NULL ,
-	UNIQUE ( sourceID )
-) ENGINE = MYISAM CHARACTER SET utf8;
