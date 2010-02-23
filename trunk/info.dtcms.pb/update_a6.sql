9b631a427e6e601cb7af646cc1bd03d0a01d1459
Alexander Ebert <alexander.ebert@deltateam-cms.info>
2010-02-23 18:11:49 +0100

    Removed SQL files since an update from previous versions is not supported

--- trunk/info.dtcms.pb/update_a6.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE pb1_1_sources ADD trustServerCert TINYINT(1) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE pb1_1_sources ADD sortOrder VARCHAR(4) NOT NULL DEFAULT '0';
-ALTER TABLE pb1_1_sources ADD INDEX sortOrder (sortOrder);
