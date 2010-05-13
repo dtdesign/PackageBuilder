21762d02a7f88e6d1880aadf3e7c5c321d170dea
Alexander Ebert <alexander.ebert@deltateam-cms.info>
2010-05-13 14:28:53 +0200

    - Rewritten PreferredPackageForm

--- trunk/com.woltlab.community.pb/files/lib/form/PreferredPackageForm.class.php
+++ trunk/com.woltlab.community.pb/files/lib/form/PreferredPackageForm.class.php
@@ -2,6 +2,7 @@
 // pb imports
 require_once(PB_DIR.'lib/data/source/Source.class.php');
 require_once(PB_DIR.'lib/system/package/PackageHelper.class.php');
+require_once(PB_DIR.'lib/system/package/SimplePackage.class.php');
 
 // wcf imports
 require_once(WCF_DIR.'lib/form/AbstractForm.class.php');
@@ -18,176 +19,161 @@ require_once(WCF_DIR.'lib/form/AbstractForm.class.php');
  */
 class PreferredPackageForm extends AbstractForm {
 	public $templateName = 'preferredPackage';
-	public $neededPermissions = 'user.source.dynamic.canUseSource';
+	public $neededPermissions = array('user.source.general.canViewSources');
+
 	// data
-	public $package = '';
+	public $cachedPackages = array();
+	public $errors = array();
+	public $filename = '';
+	public $packageDependencies = array();
 	public $packages = array();
-	public $saveSelection = false;
-	public $selectedPackages = array();
-	public $source = array();
-	public $sourceID = 0;
-	public $filename = 'pn_pv';
-	
-	public function show() {
-		WCF::getUser()->checkPermission('user.source.general.canViewSources');
-		parent::show();
-	}
-	
+	public $source = null;
+	public $requestedPackageName = '';
+	public $requestedPackageHash = '';
+	public $saveSelection = 0;
+
 	/**
-	 * @see	Page::readParameters()
+	 * @see	Form::readFormParameters()
 	 */
-	public function readParameters() {
-		parent::readParameters();
+	public function readFormParameters() {
+		parent::readFormParameters();
 
-		if (isset($_REQUEST['sourceID'])) $this->sourceID = intval($_REQUEST['sourceID']);
-		// register source
-		$this->source = new Source($this->sourceID);
-		if (!$this->source->sourceID) throw new IllegalLinkException();
-		// append sourceID
-		$this->neededPermissions .= $this->source->sourceID;
-		if (isset($_REQUEST['filename'])) $this->filename = trim($_REQUEST['filename']);
+		if (isset($_POST['filename'])) $this->filename = StringUtil::trim($_POST['filename']);
+		// read source
+		if ($this->readSource() === false) throw new IllegalLinkException();
 	}
 
 	/**
-	 * @see	Page::readData()
+	 * @see	Form::readData()
 	 */
 	public function readData() {
-		
-		
-		// get package directory
-		$directory = WCF::getSession()->getVar('source'.$this->sourceID);
-		if ($directory === null) throw new IllegalLinkException();
+		parent::readData();
 
-		// read cache
-		$packages = $this->formatCache();
+		// get available packages
+		$this->cachedPackages = $this->getCache('packages-'.$this->source->sourceID, 'Packages');
 
-		// find packageName
-		$name = null;
-		foreach ($packages as $packageName => $data) {
-			foreach ($data as $version => $directories) {
-				if (in_array($directory, $directories)) {
-					$name = $packageName;
-					break 2;
+  		// get all dependent packages
+  		$this->packageDependencies = $this->getCache('package-dependency-'.$this->source->sourceID, 'PackageDependency');
+
+		// get package information
+		$this->getRequestedPackage();
+
+    		// fetch all required packages
+    		$this->fetchDependencies($this->requestedPackageHash);
+	}
+
+	protected function fetchPackage($packageHash, $packageName, $minVersion = '') {
+		// try to find requested package
+		if (!isset($this->cachedPackages['packages'][$packageHash])) {
+			$this->errors[$packageHash] = array(
+				'message' => 'notFound',
+				'packageName' => $packageName
+			);
+
+			return;
+		}
+
+		$cachedPackage = $this->cachedPackages['packages'][$packageHash];
+		if (!empty($minVersion)) {
+			if (version_compare($minVersion, $cachedPackage['version'], '>')) {
+				$this->errors[$packageHash] = array(
+					'message' => 'insufficientVersion',
+					'packageName' => $packageName
+				);
+
+				return;
+			}
+		}
+
+		// ignore duplicate entries
+		if (isset($this->packages[$packageName]['directories'][$cachedPackage['directory']])) {
+			return;
+		}
+
+		// add current package
+		$this->packages[$packageName]['hash'] = $packageHash;
+		$this->packages[$packageName]['directories'][$cachedPackage['directory']] = $cachedPackage['version'];
+
+		$this->fetchDependencies($packageHash);
+	}
+
+	protected function fetchDependencies($packageHash) {
+		// check for dependencies
+  		if (!isset($this->packageDependencies[$packageHash])) return;
+
+  		// resolve dependencies
+		foreach ($this->packageDependencies[$packageHash] as $package) {
+			if (isset($this->cachedPackages['hashes'][$package['packageName']])) {
+				foreach ($this->cachedPackages['hashes'][$package['packageName']] as $hash) {
+					$this->fetchPackage($hash, $package['packageName'], $package['minVersion']);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Reads a given cache
+	 *
+	 * @param	string	$cacheName
+	 * @param	string	$cacheBuilder
+	 * @return	array<array>
+	 */
+	protected function getCache($cacheName, $cacheBuilder) {
+		WCF::getCache()->addResource(
+			$cacheName,
+			PB_DIR.'cache/cache.'.$cacheName.'.php',
+			PB_DIR.'lib/system/cache/CacheBuilder'.$cacheBuilder.'.class.php'
+		);
+
+		return WCF::getCache()->get($cacheName);
+	}
+
+	protected function getRequestedPackage() {
+		// get directory
+		$directory = WCF::getSession()->getVar('source'.$this->source->sourceID);
+
+		// get package name
+		if ($directory !== null) {
+			foreach ($this->cachedPackages['packages'] as $package) {
+				if ($package['directory'] == $directory) {
+					$this->requestedPackageName = $package['packageName'];
+					$this->requestedPackageHash = PackageHelper::getHash($this->source->sourceID, $package['packageName'], $directory);
+					return;
 				}
 			}
 		}
 
-		// break if packageName is unknown
-		if ($name === null) throw new IllegalLinkException();
-
-		// get hash
-  		$hash = PackageHelper::getHash($this->source->sourceID, $name, $directory);
-
-		// get all referenced packages
-		$sql = "SELECT	packageName, minVersion, file
-			FROM	pb".PB_N."_referenced_packages
-			WHERE	sourceID = ".$this->source->sourceID."
-			AND	hash = '".$hash."'
-			AND	file != ''";
-		$result = WCF::getDB()->sendQuery($sql);
-
-		while ($row = WCF::getDB()->fetchArray($result)) {
-			// continue with next package if referenced package exists
-			if (file_exists($this->source->sourceDirectory.$directory.$row['file'])) continue;
-
-			// break if we're unable to build this package
-			if (!array_key_exists($row['packageName'], $packages)) {
-				throw new SystemException('Unable to build package, cannot find referenced package "'.$row['packageName'].'"');
-			}
-
-			// register package
-			$this->packages[$row['packageName']] = array(
-				'minVersion' => $row['minVersion'],
-				'simpleHash' => sha1($row['packageName']),
-				'directories' => array()
-			);
-
-			// filter available resources by version
-			if (!empty($row['minVersion'])) {
-	   			foreach ($packages[$row['packageName']] as $version => $directories) {
-	   				if (version_compare($row['minVersion'], $version) !== 1) {
-	   					$this->packages[$row['packageName']]['directories'][$version] = $directories;
-	   				}
-	   			}
-
-	   			if (empty($this->packages[$row['packageName']]['directories'])) {
-	   				throw new SystemException('Unable to build package, no available resource for "'.$row['packageName'].'" in version "'.$row['minVersion'].'" or later found.');
-	   			}
-			}
-			else {
-				$this->packages[$row['packageName']]['directories'] = $packages[$row['packageName']];
-			}
-
-			// add default entry to package selection
-			$this->selectedPackages[$row['packageName']] = array();
-		}
-
-		// pre-select packages
-		$this->readPreSelectedPackages($directory);
+		throw new IllegalLinkException();
 	}
 
 	/**
-	 * Pre-selects directory resources if applicable
+	 * Reads-in a source
 	 *
-	 * @param	string	$directory
+	 * @return	boolean	source is valid
 	 */
-	protected function readPreSelectedPackages($directory) {
-		// no packages available for selection
-		if (empty($this->packages)) return;
+	public function readSource() {
+		if (!isset($_POST['sourceID'])) return false;
 
-		$sql = "SELECT	packageName, hash, resourceDirectory
-			FROM	pb".PB_N."_selected_packages
-			WHERE	sourceID = ".$this->source->sourceID."
-			AND	directory = '".escapeString($directory)."'";
-		$result = WCF::getDB()->sendQuery($sql);
-
-		while ($row = WCF::getDB()->fetchArray($result)) {
-   			if (array_key_exists($row['packageName'], $this->packages)) {
-       				$this->selectedPackages[$row['packageName']] = $row['resourceDirectory'];
-   			}
+		$this->source = new Source($_POST['sourceID']);
+		if ($this->source->sourceID == 0) {
+			return false;
 		}
 
-		// toggle save settings
-		if (!empty($this->selectedPackages)) $this->saveSelection = true;
+		// append source ID to permission string
+		$this->neededPermissions[] .= 'user.source.dynamic.canUseSource.'.$this->source->sourceID;
+
+		return true;
 	}
 
-	/**
-	 * Formates cache structure
-	 *
-	 * @return	array<array>
-	 */
-	protected function formatCache() {
-		$data = array();
-
-		// register cache
-		WCF::getCache()->addResource(
-			'packages-'.$this->source->sourceID,
-			PB_DIR.'cache/cache.packages-'.$this->source->sourceID.'.php',
-			PB_DIR.'lib/system/cache/CacheBuilderPackages.class.php'
-		);
-		$packages = WCF::getCache()->get('packages-'.$this->source->sourceID);
-
-		// format array structure
-		foreach ($packages as $package) {
-			$data[$package['packageName']][$package['version']][$package['directory']] = $package['directory'];
-		}
-
-		return $data;
-	}
-
-	/**
-	 * @see Page::assignVariables()
-	 */
 	public function assignVariables() {
 		parent::assignVariables();
 
 		WCF::getTPL()->assign(array(
-			'allowSpidersToIndexThisPage' => false,
+			'errors' => $this->errors,
+			'filename' => $this->filename,
 			'packages' => $this->packages,
 			'saveSelection' => $this->saveSelection,
-			'selectedPackages' => $this->selectedPackages,
-			'source' => $this->source,
-			'filename' => $this->filename
+			'source' => $this->source
 		));
 	}
 }
