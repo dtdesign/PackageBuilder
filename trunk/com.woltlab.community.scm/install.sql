6f95f2ed1efb60fbaf7150aa98308e3cc4e22f9a
TimWolla <timwolla@arcor.de>
2010-06-24 21:12:08 +0200

    Added forgotten braces

--- trunk/com.woltlab.community.scm/install.sql
+++ trunk/com.woltlab.community.scm/install.sql
@@ -1,6 +1,6 @@
 CREATE TABLE wcf1_scm (
 	packageID int(10) unsigned NOT NULL
 	scm VARCHAR(255) NOT NULL,
-	PRIMARY KEY packageID,
+	PRIMARY KEY ( packageID ),
 	UNIQUE KEY scm (scm)
 ) ENGINE=MyISAM CHARACTER SET=utf8;
