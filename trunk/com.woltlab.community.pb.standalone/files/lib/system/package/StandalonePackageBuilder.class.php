ed65081612314c4103aa8e5e0568552141b9ddf4
Alexander Ebert <ebert@woltlab.com>
2011-03-11 20:04:07 +0100

    - Rewrote mechanism to create a WCFSetup-archive (it is now less dynamically hence a specific structure is enforced)

--- trunk/com.woltlab.community.pb.standalone/files/lib/system/package/StandalonePackageBuilder.class.php
+++ trunk/com.woltlab.community.pb.standalone/files/lib/system/package/StandalonePackageBuilder.class.php
@@ -9,175 +9,132 @@ require_once(WCF_DIR.'lib/system/io/ZipWriter.class.php');
 /**
  * Builds a package.
  *
- * @author	Tim Düsterhus
- * @copyright	2009-2010 WoltLab Community
+ * @author	Tim Düsterhus, Alexander Ebert
+ * @copyright	2009-2011 WoltLab Community
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.woltlab.community.pb
  * @subpackage	package
  * @category 	PackageBuilder
  */
-class PackageBuilder {
-	private $archive = null;
-	private $excludeFiles = array('.', '..');
-	private $ignoreDoFiles = true;
-	private $filename = '';
-	private $location = '';
-	private $package = array();
-	private $source = null;
-
+class StandalonePackageBuilder {
 	/**
-	 * Builds a package
-	 *
-	 * @param	integer	$source			id or instance of a source
-	 */
-	public function __construct($filename, $source, Array $packages, Array $readMeFiles, $dataBaseStructure, Array $setupLanguages, Array $licenses, Array $templates, $files, Array $languages) {
-		$this->source = ($source instanceof Source) ? $source : new Source($source);
-		
-		$buildDirectory = $this->source->buildDirectory.'/';
-		foreach($packages as $package) {
-			if(!file_exists($buildDirectory.$package)) throw new SystemException('Package "'.$package.'" not found');
-		}
-		
-		foreach($readMeFiles as $readMe) {
-			if(!file_exists($readMe)) throw new SystemException('Readme "'.basename($readMe).'" not found');
-		}
-		
-		if(!file_exists($dataBaseStructure)) throw new SystemException('DataBaseStructure not found');
-		
-		foreach($setupLanguages as $setupLanguage) {
-			if(!file_exists($setupLanguage)) throw new SystemException('SetupLanguage "'.basename($setupLanguage).'" not found');
-		}
-		
-		foreach($licenses as $license) {
-			if(!file_exists($licenses)) throw new SystemException('License "'.basename($license).'" not found');
-		}
-		
-		foreach($templates as $template) {
-			if(!file_exists($template)) throw new SystemException('Template "'.basename($template).'" not found');
-		}
-		if(!file_exists($files)) throw new SystemException('Files not found');
-		
-		foreach($languages as $language) {
-			if(!file_exists($language)) throw new SystemException('Language "'.basename($language).'" not found');
-		}
-		$data = array(
-			'pn' => $this->package['name'],
-			'pv' => $this->package['version'],
-			'pr' => 'r'.$this->source->revision,
-			't' => 	DateUtil::formatTime('%D %T', TIME_NOW, false)
-		);
-		$this->filename = StandalonePackageHelper::getArchiveName($filename, $data);
-		$location = $buildDirectory.'standalone/'.$this->filename;
-		
-		// intialize archive
-		$this->location = $this->createArchive($location, $packages, $readMeFile, $dataBaseStructure, $setupLanguages, $licenses, $templates, $files, $languages);
-	}
-
-	/**
-	 * Creates complete archive.
-	 *
-	 * @param	string	$directory
-	 * @param	string	$filename
-	 */
-	public function createArchive($location, $packages, $readMeFile, $dataBaseStructure, $setupLanguages, $licenses, $templates, $files, $languages) {
-		$baseDir = dirname($location);
-		
-		$package = new TarWriter($baseDir.'/WCFSetup.tar.gz', true);
-		FileUtil::makePath($baseDir.'/tmp/upload/setup/db');
-		FileUtil::makePath($baseDir.'/tmp/upload/setup/lang');
-		FileUtil::makePath($baseDir.'/tmp/upload/setup/license');
-		FileUtil::makePath($baseDir.'/tmp/upload/setup/template');
-		FileUtil::makePath($baseDir.'/tmp/upload/install/lang');
-		FileUtil::makePath($baseDir.'/tmp/upload/install/packages');
-		copy($dataBaseStructure, $baseDir.'/tmp/upload/setup/db/mysql.sql');
-		foreach($setupLanguages as $setupLanguage) {
-			copy($setupLanguage, $baseDir.'/tmp/upload/setup/lang/'.basename($setupLanguage));
-		}
-		foreach($licenses as $license) {
-			copy($license, $baseDir.'/tmp/upload/setup/license/'.basename($license));
-		}
-		foreach($templates as $template) {
-			copy($template, $baseDir.'/tmp/upload/setup/template/'.basename($template));
-		}
-		self::copyDirectory($files, $baseDir.'/tmp/upload/install/files');
-		foreach($languages as $language) {
-			copy($language, $baseDir.'/tmp/upload/install/lang/'.basename($language));
-		}
-		foreach($packages as $package) {
-			copy($package, $baseDir.'/tmp/upload/install/packages/'.basename($package));
-		}
-		$package->add(array($baseDir.'/tmp/upload/setup', $baseDir.'/tmp/upload/install'), '', $baseDir.'/tmp/upload');
-		
-		$file = new ZipWriter();
-		
-		// add readmes
-		foreach($readMeFiles as $readMe) {
-			$file->addFile(file_get_contents($readMe), basename($readMe), filemtime($readMe));
-		}
-		$file->addDir('upload');
-		$file->addFile(file_get_contents($baseDir.'/WCFSetup.tar.gz'), 'upload/WCFSetup.tar.gz', filemtime($baseDir.'/WCFSetup.tar.gz'));
-		
-		file_put_contents($location, $file->getFile());
-		$dir = DirectoryUtil::getInstance($baseDir.'/tmp');
-		$dir->removeAll();
-		unlink($baseDir.'/WCFSetup.tar.gz');
-		
-		return $location;
-	}
-	
-	/**
-	 * Copies a directory.
+	 * location of WCFSetup.tar.gz
 	 * 
-	 * @param	string		$sourceDir
-	 * @param	string		$destinationDir
-	 * @return	integer
+	 * @var	string
 	 */
-	public static function copyDirectory($sourceDir, $destinationDir) {
-		$num = 0;
-		
-		if (!is_dir($destinationDir)) FileUtil::makePath($destinationDir);
-		
-		if ($directory = opendir($sourceDir)) {
-			while ($file = readdir($directory)) {
-				if ($file != '.' && $file != '..') {
-					$sourceFile = $sourceDir . '/' . $file;
-					$destinationFile = $destinationDir . '/' . $file;
-					
-					if (is_file($sourceFile)) {
-						if (is_file($destinationFile)) {
-							$ow = filemtime($sourceFile) - filemtime($destinationFile);
-						}
-						else {
-							$ow = 1;
-						}
-						
-						if ($ow > 0) {
-							if (copy($sourceFile, $destinationFile)) {
-								touch($destinationFile, filemtime($sourceFile)); $num++;
-							}
-							else {
-								throw new SystemException('Error: File "'.$sourceFile.'" could not be copied!');
-							}
-						}
-					}
-					else if (is_dir($sourceFile)) {
-						$num += self::copyDirectory($sourceFile, $destinationFile);
-					}
-				}
-			}
-			closedir($directory);
-		}
-		
-		return $num;
+	protected $path = '';
+	
+	/**
+	 * resource directory containing complete data for a WCFSetup
+	 * 
+	 * @var	string
+	 */
+	protected $resourceDirectory = '';
+	
+	/**
+	 * Source object
+	 * 
+	 * @var	Source
+	 */
+	protected $source = null;
+	
+	/**
+	 * Initializes a new WCFSetup-build
+	 *
+	 * @param	Source	$source
+	 * @param	string	$resourceDirectory
+	 */
+	public function __construct(Source $source, $resourceDirectory) {
+		$this->source = $source;
+		$this->resourceDirectory = FileUtil::addTrailingSlash($resourceDirectory);
 	}
 	
+	/**
+	 * Builds a WCFSetup.
+	 * 
+	 * @param	array	$packages
+	 * @param	string	$outputDirectory
+	 */
+	public function createWcfSetup(array $packages, $outputDirectory = '') {
+		// ensure output directory is set and exists
+		if (empty($outputDirectory)) {
+			$outputDirectory = $this->source->buildDirectory;
+		}
+		else if (!is_dir($outputDirectory)) {
+			FileUtil::makePath($outputDirectory);
+		}
+		$outputDirectory = FileUtil::addTrailingSlash($outputDirectory);
+		
+		// create temporarily directory
+		$hash = StringUtil::getRandomID();
+		$buildDirectory = $outputDirectory . $hash . '/';
+		
+		// populate install directory
+		$this->cloneDirectory($buildDirectory, 'install/files');
+		$this->cloneDirectory($buildDirectory, 'install/lang');
+		
+		// populate setup directory
+		$this->cloneDirectory($buildDirectory, 'setup/db');
+		$this->cloneDirectory($buildDirectory, 'setup/lang');
+		$this->cloneDirectory($buildDirectory, 'setup/license');
+		$this->cloneDirectory($buildDirectory, 'setup/template');
+		
+		// copy packages
+		foreach ($packages as $package) {
+			if (!file_exists($package)) {
+				throw new SystemException("Required package '".$package."' not found.");
+			}
+			
+			copy ($package, $buildDirectory . 'install/packages/' . basename($package));
+		}
+		
+		// create wcf setup
+		$wcfSetup = new TarWriter($outputDirectory . 'WCFSetup.tar.gz', true); 
+		$wcfSetup->add(array($buildDirectory . 'install', $buildDirectory . 'install'), '', $buildDirectory);
+		$wcfSetup->create();
+		
+		$this->path = $outputDirectory . 'WCFSetup.tar.gz';
+	}
+	
+	/**
+	 * Clones a directory.
+	 * 
+	 * @param	string		$buildDirectory
+	 * @param	string		$path
+	 */
+	protected function cloneDirectory($buildDirectory, $path) {
+		// ensure source directory exists
+		if (!is_dir($this->resourceDirectory . $path)) {
+			throw new SystemException("Required path '".$path."' within resource directory is not available.");
+		}
+		
+		// create path
+		if (!is_dir($buildDirectory . $path)) {
+			FileUtil::makePath($buildDirectory . $path);
+		}
+		
+		// copy files recursively
+		$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->resourceDirectory . $path));
+		while ($it->valid()) {
+			if (!$it->isDot()) {
+				if (!is_dir($buildDirectory . $path . $it->getSubPath())) {
+					FileUtil::makePath($buildDirectory . $path . $it->getSubPath());
+				}
+				
+				copy ($it->key(), $buildDirectory . $path . $it->getSubPathName());
+			}
+			
+			$it->next();
+		}
+	}
+
 	/**
 	 * Returns the archive location
 	 *
 	 * @return	string
 	 */
 	public function getArchiveLocation() {
-		return $this->location;
+		return $this->path;
 	}
 }
 ?>
